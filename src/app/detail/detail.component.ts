fd11e161a3206bc65a1d28ed1ee894bedc161790
arik92 <arik92@gmail.com>
2020-08-25 12:00:40 +0300

    tweak projects

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -11,12 +11,29 @@ import { Subscription } from 'rxjs';
 export class DetailComponent implements OnInit, OnDestroy {
   detailItem: any; // can be converted to interface
   routeSubscription: Subscription;
+  projectStatus: number = 0;
   constructor(private route: ActivatedRoute, private content: ContentService) { }
 
   ngOnInit() {
     this.routeSubscription = this.route.paramMap.subscribe((paramsMap:any) => {
       this.detailItem = this.content.getProject(paramsMap.params.title);
+      console.log('detail item is ', this.detailItem);
+      this.determineStatus(this.detailItem.status);
     });
+  }
+
+  determineStatus(status: string) {
+    switch (status) {
+      case 'DEV':
+        this.projectStatus = 1;
+        break;
+      case 'DONE':
+      this.projectStatus = 2;
+      break;
+      case 'UNM':
+        this.projectStatus = 3;
+        break;
+    }
   }
 
   ngOnDestroy() {
